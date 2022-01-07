70c0da17d2463b0935930c0cddf66c97cc21b190
mac-madison <macanmadison@gmail.com>
2022-01-07 15:02:33 -0800

    tidy up

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,17 +8,6 @@ const Board = ({ board, deleteBoard }) => {
 
   return (
     <>
-      {/* <div class="card border-primary mb-3" style={{ maxWidth: 300 }}>
-        <div class="card-header">Header</div>
-        <div class="card-body">
-          <h4 class="card-title">Primary card title</h4>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div> */}
-
       <Card
         className="card border-primary mb-3"
         style={{ remSize, marginTop: 15 }}
